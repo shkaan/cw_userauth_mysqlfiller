0c13e2f2224da68d346a49274e11da5c1b17ad1b
shkaan <shkaan@gmail.com>
2016-05-10 00:59:37 +0200

    make it better

--- db.js
+++ db.js
@@ -1,14 +1,7 @@
 
 var knex = require('knex')({
     client: "mysql",
-    connection: {
-        host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',  // your host
-        user: 'iyc4qnjpwpyk0rs0', // your database username
-        password: 'y0llnux5zzuzu3kr ', // your database password
-        database: 'k2yzot55d4yv5e7l', //SCHEMA to use
-        charset: 'UTF8_GENERAL_CI'
-
-    }
+    connection: process.env.JAWSDB_URL
 });
 var DB = require('bookshelf')(knex);
 
